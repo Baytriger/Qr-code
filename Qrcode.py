d9bdeab9e960ce03503051a6855678cd4bfd5b84
Mohammed-Moshood <mohammedmoshood100@gmail.com>
2025-08-08 10:27:30 +0100

    Add files via upload

---
+++ Qrcode.py
@@ -0,0 +1,12 @@
+import qrcode
+from PIL import Image
+
+website_link = 'https://nnpcgroup.com/'
+
+qr = qrcode.QRCode(version=4, box_size=5, border=5)
+qr.add_data(website_link)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color='green', back_color='white')
+img.save('nnpc_qr.png')
+img.show()
