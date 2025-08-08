c9762db9ba9e530d46264c435038ba0e25786767
Mohammed-Moshood <mohammedmoshood100@gmail.com>
2025-08-08 10:28:22 +0100

    Update Qrcode.py

--- Qrcode.py
+++ Qrcode.py
@@ -1,7 +1,7 @@
 import qrcode
 from PIL import Image
 
-website_link = 'https://nnpcgroup.com/'
+website_link = 'https://github.com/'
 
 qr = qrcode.QRCode(version=4, box_size=5, border=5)
 qr.add_data(website_link)
@@ -10,3 +10,4 @@ qr.make(fit=True)
 img = qr.make_image(fill_color='green', back_color='white')
 img.save('nnpc_qr.png')
 img.show()
+
